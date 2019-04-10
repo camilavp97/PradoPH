2cfc53e2358447746f8282b8fa0e46e8a709a926
jarangoc1997 <juan_arango23151@elpoli.edu.co>
2019-04-09 20:27:21 -0500

    1.Creación de las entidades de perfil de usuario y de usuario, en la clase usuario hay un ejemplo de como debe de ir la relación entre tablas.

---
+++ pradoph/src/main/java/com/ppi/prado/ph/entidades/Usuario.java
@@ -0,0 +1,107 @@
+package com.ppi.prado.ph.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@ManyToOne
+	private PerfilUsuario perfilUsuario;
+	
+	@NotEmpty
+	@Column
+	private String nombre;
+	
+	@NotEmpty
+	@Column
+	private String apellido;
+	
+	@Column
+	private String telefono;
+	
+	@Column
+	private String fotoPerfil;
+
+	@Column
+	@NotEmpty
+	private String usuario;
+
+	@Column
+	@NotEmpty
+	private String clave;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public PerfilUsuario getPerfilUsuario() {
+		return perfilUsuario;
+	}
+
+	public void setPerfilUsuario(PerfilUsuario perfilUsuario) {
+		this.perfilUsuario = perfilUsuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getFotoPerfil() {
+		return fotoPerfil;
+	}
+
+	public void setFotoPerfil(String fotoPerfil) {
+		this.fotoPerfil = fotoPerfil;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
+}
