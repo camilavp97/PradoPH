914d4ed20f2fdfee1e6a0c13a101883a155807bf
diegoarango <juan_arango23151@elpoli.edu.co>
2019-04-18 11:40:56 -0500

    Creación de las entidades propiedad y detalleUsuarioPropiedad

---
+++ pradoph/src/main/java/com/ppi/prado/ph/entidades/DetalleUsuarioPropiedad.java
@@ -0,0 +1,46 @@
+package com.ppi.prado.ph.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class DetalleUsuarioPropiedad {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@ManyToOne
+	private Usuario usuario;
+	
+	@ManyToOne
+	private Propiedad propiedad;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Propiedad getPropiedad() {
+		return propiedad;
+	}
+
+	public void setPropiedad(Propiedad propiedad) {
+		this.propiedad = propiedad;
+	}
+	
+}
---
+++ pradoph/src/main/java/com/ppi/prado/ph/entidades/Propiedad.java
@@ -0,0 +1,84 @@
+package com.ppi.prado.ph.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Propiedad {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	@NotEmpty
+	private String nombrePropiedad;
+	
+	@Column
+	@NotEmpty
+	private String nit;
+	
+	@Column
+	private String fotoPerfil;
+	
+	@Column
+	@NotEmpty
+	private String telefono;
+	
+	@Column
+	@NotEmpty
+	private String direccion;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombrePropiedad() {
+		return nombrePropiedad;
+	}
+
+	public void setNombrePropiedad(String nombrePropiedad) {
+		this.nombrePropiedad = nombrePropiedad;
+	}
+
+	public String getNit() {
+		return nit;
+	}
+
+	public void setNit(String nit) {
+		this.nit = nit;
+	}
+
+	public String getFotoPerfil() {
+		return fotoPerfil;
+	}
+
+	public void setFotoPerfil(String fotoPerfil) {
+		this.fotoPerfil = fotoPerfil;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+}
