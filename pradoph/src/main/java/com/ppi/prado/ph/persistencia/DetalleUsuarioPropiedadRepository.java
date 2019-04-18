384d7940716f67f57bf3aaa98cdf5c31dd2b801a
diegoarango <juan_arango23151@elpoli.edu.co>
2019-04-18 11:44:05 -0500

    Creación de las interfaces de persistencia para la entidad Propiedad y DetalleUsuarioPropiedad

---
+++ pradoph/src/main/java/com/ppi/prado/ph/persistencia/DetalleUsuarioPropiedadRepository.java
@@ -0,0 +1,9 @@
+package com.ppi.prado.ph.persistencia;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ppi.prado.ph.entidades.DetalleUsuarioPropiedad;
+
+public interface DetalleUsuarioPropiedadRepository extends CrudRepository<DetalleUsuarioPropiedad, Long>{
+
+}
